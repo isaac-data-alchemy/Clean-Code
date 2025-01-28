c2db5a673561c36bebf2905b3a65f1de757749a5
isaac-data-alchemy <ejimoforsystemtech123@gmail.com>
2025-01-28 18:29:32 +0100

    updated the documentation

--- search/get_item.py
+++ search/get_item.py
@@ -15,17 +15,17 @@ def get_item(stack:list, query:str) -> str:
 def get_itemv2(stack:list, query:str) -> str:
     """
         Method: 'get_itemv2()' checks for a str in a list and returns a str
-        Arguments: requires two positional arguments 'stack' and 'list'
+        Arguments: requires two positional arguments 'stack' and 'query'
         Parameters:
-        Stack: a represents a list [] object
-        Item: represents a str '' object
+        Stack: a list [] object representing the database storing the user's requested objects
+        query: a str '' object representing the user's requested object
         Description: iterates through the stack and then through each stack within the stack
-            running a '==' check for the 'item' within each 'stack'
-        Returns:  if 'item' is found within this list it returns the item as a str
+            running a '==' check for the 'query' within each 'stack'
+        Returns:  if 'query' is found within this list it returns the query as a str
         """
     for object in stack:
-        for i in object:
-            if query== i:
+        for q in object:
+            if query== q:
                 return query
 
     
