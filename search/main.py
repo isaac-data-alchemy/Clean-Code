836d9b5ed5ad1cf2a1072dd877c39a8e28a042a0
isaac-data-alchemy <ejimoforsystemtech123@gmail.com>
2025-02-18 11:00:34 +0100

    modified access point main.py

---
+++ search/main.py
@@ -0,0 +1,10 @@
+from seek import search_v4 as search
+def main():
+    names_1d = ['ella', 'david', 'tipulate', 'sharon', 'amaka', 'jordana']
+    names_2d = [['ella', 'david'], ['tipulate', 'sharon'], ['amaka', 'jordana']]
+    text = "Enter a name to search: "
+    search(names_1d, "Enter any term to search: ")
+
+
+if __name__ == "__main__":
+    main()
