c1ecaca91c7c16b26632f3dd20d0586a3ebd0537
isaac-data-alchemy <ejimoforsystemtech123@gmail.com>
2025-02-18 13:33:42 +0100

    commented out unused code

--- search/main.py
+++ search/main.py
@@ -1,7 +1,7 @@
 from seek import search_v4 as search
 def main():
     names_1d = ['ella', 'david', 'tipulate', 'sharon', 'amaka', 'jordana']
-    names_2d = [['ella', 'david'], ['tipulate', 'sharon'], ['amaka', 'jordana']]
+#    names_2d = [['ella', 'david'], ['tipulate', 'sharon'], ['amaka', 'jordana']]
     search(names_1d, "Enter any term to search: ")
 
 
