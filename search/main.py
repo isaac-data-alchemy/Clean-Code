7fe3fdf7ee1409a40145ff25b39b590b4d35512a
isaac-data-alchemy <ejimoforsystemtech123@gmail.com>
2025-02-18 11:26:19 +0100

    deleted global var Text

--- search/main.py
+++ search/main.py
@@ -2,7 +2,6 @@ from seek import search_v4 as search
 def main():
     names_1d = ['ella', 'david', 'tipulate', 'sharon', 'amaka', 'jordana']
     names_2d = [['ella', 'david'], ['tipulate', 'sharon'], ['amaka', 'jordana']]
-    text = "Enter a name to search: "
     search(names_1d, "Enter any term to search: ")
 
 
